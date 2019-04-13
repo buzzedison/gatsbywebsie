aad8a89a32e4f9819cf4202bb79782818176cdca
Edison Ade <ask@buzzedison.com>
2019-04-14 00:53:37 +0100

    blog try again

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,47 +1,23 @@
-const path = require(`path`)
+const Promise = require("bluebird")
+const path = require("path")
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
+
   return new Promise((resolve, reject) => {
     const blogPost = path.resolve("./src/templates/blogPost.js")
     resolve(
-      graphql(
-        `
-          {
-            allContentfulInsight
-              
-            {
-              edges{
-                node{
-                  title
-                  slug
-                  image{
-                    fluid{
-                      src
-                    }
-                  }
-                body{
-                  childContentfulRichText{
-                    html
-                  }
-                }
-                }
-              }
-          }
-        `
-      ).then(result => {
+      graphql().then(result => {
         if (result.errors) {
+          console.log(result.errors)
           reject(result.errors)
         }
 
-        // Create pages for each markdown file.
-        const post = result.data.ContentfulInsight.edges
-        posts.forEach(({ node }) => {
+        const posts = result.data.allContentfulInsight.edges
+        posts.forEach((post, index) => {
           createPage({
-            path: `/insight/${post.node.slug}/`,
+            path: `/blog/${post.node.slug}/`,
             component: blogPost,
-            // In your blog post template's graphql query, you can use path
-            // as a GraphQL variable to query for data from the markdown file.
             context: {
               slug: post.node.slug,
             },
