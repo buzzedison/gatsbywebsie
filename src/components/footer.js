e084e4736642fad6da46644e722c7d389fa45666
Edison Ade <ask@buzzedison.com>
2019-04-28 03:04:40 +0100

    removed footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,26 +1,7 @@
 import React from "react"
-import { BrowserRouter } from "react-router-dom"
-import { MDBNav, MDBNavItem, MDBNavLink } from "mdbreact"
 
 const Footer = () => {
-  return (
-    <BrowserRouter>
-      <MDBNav className="justify-content-center" color="amber">
-        <MDBNavItem>
-          <MDBNavLink to="#!">Home</MDBNavLink>
-        </MDBNavItem>
-        <MDBNavItem>
-          <MDBNavLink to="#!">Courses</MDBNavLink>
-        </MDBNavItem>
-        <MDBNavItem>
-          <MDBNavLink to="#!">Insight</MDBNavLink>
-        </MDBNavItem>
-        <MDBNavItem>
-          <MDBNavLink to="#!">Events</MDBNavLink>
-        </MDBNavItem>
-      </MDBNav>
-    </BrowserRouter>
-  )
+  return <div>Test</div>
 }
 
 export default Footer
