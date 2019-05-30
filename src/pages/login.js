1776086b065e888bbbdfddd014280342951d857f
Edison Ade <ask@buzzedison.com>
2019-05-30 18:21:36 +0100

    netlify login page

---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import Login from "../components/account"
+const Account = () => {
+  return (
+    <Layout>
+      <Login />
+    </Layout>
+  )
+}
+
+export default Account
